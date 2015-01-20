00af5f04438790ca945836977757e0a9427010da
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2015-01-20 10:09:46 +0700

    fix Bug of relationships between links, users and comments... and add development  database

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
     @link = Link.find(params[:link_id])
     @comment = @link.comments.new(comment_params)
     @comment.user = current_user
+    @comment.link = @link 
 
     respond_to do |format|
       if @comment.save
