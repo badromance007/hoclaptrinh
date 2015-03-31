f6bb1f0fed881a6b8a89c6aebd7279e88b80c354
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2015-03-31 14:48:06 +0700

    Fixed bugs: 'title of link must not be blank'

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, presence: true
 
   acts_as_taggable
 end
