be01a08154ce33029ab2f1ca05f511b91f59696e
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2015-03-31 17:36:54 +0700

    comment must have at least 4 characters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 4, :message => 'Bình luận phải có ít nhất 4 ký tự trở lên!!!!!!' }
   validates :user_id, presence: true
   validates :link_id, presence: true
+
 end
